b606b749e8d29612f3a42371ea49e3410da89501
akshay nagargoje <akshaynagargoje0716@gmail.com>
2024-11-02 13:21:49 +0530

    latest chnages added

--- data/index.ts
+++ data/index.ts
@@ -44,7 +44,7 @@ export const gridItems = [
     imgClassName: "",
     titleClassName: "justify-start",
     img: "grid.svg",
-    spareImg: "/b4.svg",
+    spareImg: "b4.svg",
   },
 
   {
@@ -55,7 +55,7 @@ export const gridItems = [
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "b5.svg",
-    spareImg: "/grid.svg",
+    spareImg: "grid.svg",
   },
   {
     id: 6,
@@ -75,7 +75,7 @@ export const projects = [
     title: "3D Solar System Planets to Explore",
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
     img: "p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    iconLists: ["re.svg", "tail.svg", "ts.svg", "three.svg", "fm.svg"],
     link: "/ui.earth.com",
   },
   {
@@ -83,7 +83,7 @@ export const projects = [
     title: "Yoom - Video Conferencing App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    iconLists: ["next.svg", "tail.svg", "ts.svg", "stream.svg", "c.svg"],
     link: "/ui.yoom.com",
   },
   {
@@ -91,7 +91,7 @@ export const projects = [
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
     img: "p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    iconLists: ["re.svg", "tail.svg", "ts.svg", "three.svg", "c.svg"],
     link: "/ui.aiimg.com",
   },
   {
@@ -99,7 +99,7 @@ export const projects = [
     title: "Animated Apple Iphone 3D Website",
     des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
     img: "p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    iconLists: ["next.svg", "tail.svg", "ts.svg", "three.svg", "gsap.svg"],
     link: "/ui.apple.com",
   },
 ];
@@ -142,31 +142,31 @@ export const companies = [
     id: 1,
     name: "cloudinary",
     img: "cloud.svg",
-    nameImg: "/cloudName.svg",
+    nameImg: "cloudName.svg",
   },
   {
     id: 2,
     name: "appwrite",
     img: "app.svg",
-    nameImg: "/appName.svg",
+    nameImg: "appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
     img: "host.svg",
-    nameImg: "/hostName.svg",
+    nameImg: "hostName.svg",
   },
   {
     id: 4,
     name: "stream",
     img: "s.svg",
-    nameImg: "/streamName.svg",
+    nameImg: "streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
     img: "dock.svg",
-    nameImg: "/dockerName.svg",
+    nameImg: "dockerName.svg",
   },
 ];
 
@@ -176,28 +176,28 @@ export const workExperience = [
     title: "Frontend Engineer Intern",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    thumbnail: "exp1.svg",
   },
   {
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    thumbnail: "exp2.svg",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    thumbnail: "exp3.svg",
   },
   {
     id: 4,
     title: "Lead Frontend Developer",
     desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    thumbnail: "exp4.svg",
   },
 ];
 
